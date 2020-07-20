296446d13215de361a714ba6d85ee82c756d828e
Daniel Trevino <danytq00@gmail.com>
2020-07-20 14:17:39 -0500

    Create react app with main pages

---
+++ ubg/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Room from './pages/Room'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/:gameId">
+          <Game/>
+        </Route>
+        <Route exact path="/:gameId/:roomId">
+          <Room/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
