507a4162bbf96bbd175a09778473e31b06b69461
Shandy Kim <shandyjkim@gmail.com>
2020-07-22 12:49:46 -0700

    Change imports from double to single quotes

--- ubg/src/Reviews/Review.js
+++ ubg/src/Reviews/Review.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
 
 const useStyles = makeStyles(theme => ({
