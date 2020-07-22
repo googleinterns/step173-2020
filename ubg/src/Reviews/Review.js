91e628092bbb17303c55f9f7bbb8c44db060d2c9
Shandy Kim <shandyjkim@gmail.com>
2020-07-22 11:59:22 -0700

    Add styling for review section

--- ubg/src/Reviews/Review.js
+++ ubg/src/Reviews/Review.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import {
+  Box,
+  ListItem,
+  Divider,
+  ListItemText,
+  Typography
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Rating from '@material-ui/lab/Rating';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  fonts: {
+    fontWeight: "bold"
+  },
+  inline: {
+    display: "inline"
+  }
+}));
 
 // Returns one formatted review
 const Review = props => {
-    return (
-      <div>
-        <h6>{props.review.name}</h6>
-        <p>Rating: {props.review.rating}</p>
-        <p> {props.review.text} </p>
-        <hr />
+  const classes = useStyles();
+  const value = parseInt(props.review.rating, 10);
+  return (
+    <div>
+        <ListItem key={props.review.name} alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Typography className={classes.fonts} component={'span'}>
+                {props.review.name}
+              </Typography>
+            }
+            secondary={
+              <>
+                {/* <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  {props.review.rating}
+                </Typography> */}
+                <Rating name="read-only" value={parseInt(props.review.rating, 10)} readOnly />
+                <br />
+                <Typography component={'span'}>
+                    {props.review.text}
+                </Typography>
+              </>
+            }
+          />
+        </ListItem>
+        <Divider />
       </div>
     );
   };
