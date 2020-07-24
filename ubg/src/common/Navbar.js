dafad651cefe73b377a1516a85d71404042d5d4f
zhangshyue <shiyuez@google.com>
2020-07-24 10:58:34 -0500

    add search to Navbar

--- ubg/src/common/Navbar.js
+++ ubg/src/common/Navbar.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import AuthButtons from './AuthButtons';
+import Search from './Search';
 
 const useStyles = makeStyles((theme) => ({
     menuButton: {
@@ -27,6 +28,7 @@ export default function Navbar() {
           <Typography variant="h6" className={classes.title}>
             UltimateBoardGame
           </Typography>
+          <Search />
           <AuthButtons />
         </Toolbar>
       </AppBar>
