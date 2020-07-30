557875b9c296628561ef6e2f79b61074520985d1
Shandy Kim <shandyjkim@gmail.com>
2020-07-30 11:24:30 -0700

    Add styling to title

--- ubg/src/pages/Home.js
+++ ubg/src/pages/Home.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Navbar from '../common/Navbar';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    justify: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+}));
 
 /**
  * @return {ReactElement} Home page which is also landing page
  */
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <div>
       <Navbar/>
-      <h1>
-        Welcome to UltimateBoardGame!!!
-      </h1>
+      <Box
+        className={classes.box}
+        container='true'
+        m={10}
+      >
+        <Typography
+          className={classes.header}
+          variant='h1'
+        >
+          Welcome to UltimateBoardGame!
+        </Typography>
+      </Box>
     </div>
   );
 }
