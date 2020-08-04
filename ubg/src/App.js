718aaaec802ba0d9afbe9c1d5c3f25b94cebed9b
zhangshyue <shiyuez@google.com>
2020-08-04 11:24:16 -0500

    fix bug

--- ubg/src/App.js
+++ ubg/src/App.js
@@ -35,9 +35,9 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            {/* <Route exact path="/search">
+            <Route exact path="/search">
               <Search />
-            </Route> */}
+            </Route>
             <Route exact path="/search/:query">
               <Search />
             </Route>
