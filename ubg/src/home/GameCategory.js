816e2c7acfebe53ca6bdaad08531406813dcad38
zhangshyue <shiyuez@google.com>
2020-08-05 14:44:55 -0500

    add corousel and added more categories of games

--- ubg/src/home/GameCategory.js
+++ ubg/src/home/GameCategory.js
@@ -1,102 +1,46 @@
-import React, {useEffect} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React from 'react';
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import GameCard from '../search/GameCard';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMore from '@material-ui/icons/ExpandMore';
+import Carousel from 'react-elastic-carousel';
 
-const useStyles = makeStyles((theme) => ({
-  expand: {
-        marginLeft: 'calc(50% - 23px)',
-    marginRight: '50vw',
-  },
-//   header: {
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-}));
+const breakPoints = [
+  {width: 1, itemsToShow: 1},
+  {width: 600, itemsToShow: 2},
+  {width: 960, itemsToShow: 3},
+  {width: 1280, itemsToShow: 4},
+  {width: 1920, itemsToShow: 6}
+];
 
 /**
  * @return {ReactElement} Home page which is also landing page
  */
 export default function GameCategory({category, games}) {
-  const classes = useStyles();
-  const [expand, setExpand] = React.useState(false);
-  const [display, setDisplay] = React.useState('none');
-
-  // function modifyDisplay() {
-  //   // console.log(expand);
-  //   console.log(display);
-  //   if (expand === true) {
-  //     setDisplay('block');
-  //   } else {
-  //     setDisplay('none');
-  //   }
-  // }
-  // useEffect(modifyDisplay, [expand]);
-
-  const handleExpandClick = () => {
-    setExpand(!expand);
-    console.log('click');
-  };
-
-  function loadGameCard(item, index) {
-    let display = 'none';
-    if (expand === true) {
-      display = 'block';
-    }
-    
-    let displayCard;
-    // if (expand === false) {
-    if (index < 3) {
-      displayCard = 'block';
-    // } else if (index === 2) {
-    //   display = { xs: 'none',md: 'none', lg: 'block' };
-    } else if (index === 3) {
-      displayCard = { xs: 'block' ,md: display, lg: 'block' };
-    } else if (index < 6) {
-      displayCard = { xs: display, md: display, lg: display, xl: 'block' };
-    } else {
-      displayCard = display;
-    }
-  // } else {
-  //   displayCard = 'block';
-  // }
-    const component= <Grid key={item['id']} item xs={12} sm={6} xl={2} lg={3} md={4}>
-      <Box display={displayCard}>
-      <GameCard id={item['id']}
-        image={item['image']}
-        name={item['Name']}
-        year={item['year']}
-        minTime={item['minPlaytime']}
-        maxTime={item['maxPlaytime']}
-        minPlayer={item['minPlayer']}
-        maxPlayer={item['maxPlayer']}
-        rating={item['rating']}
-        minAge={item['minAge']}
-        weight={item['weight']} /></Box>
-    </Grid>
-    if (index === 5) {
-      console.log(component);
-    }
-    
-    return component
-  }
-
   return (
-    <Box m={10}>
+    <Box m={10} container='true'>
       <h1>{category}</h1>
-      <Grid container justify="flex-start" alignItems="stretch" spacing={4}>
-      {games.map((item, index) => loadGameCard(item, index)
-        )}
-        {/* <div onClick={() => setExpand(!expand)}> */}
-        {/* <div onClick={console.log('click')}> */}
-        <IconButton className={classes.expand} onClick={handleExpandClick}>
-          <ExpandMore />
-        </IconButton>
-        {/* </div> */}
-        </Grid>
+      <Carousel
+          itemPadding={[10, 15]}
+          breakPoints={breakPoints}
+        >
+          {games.map((game) => {
+            return (
+              <GameCard 
+                key={game.id}
+                id={game.id}
+                image={game.image}
+                name={game.Name}
+                year={game.year}
+                minTime={game.minPlaytime}
+                maxTime={game.maxPlaytime}
+                minPlayer={game.minPlayer}
+                maxPlayer={game.maxPlayer}
+                rating={game.rating}
+                minAge={game.minAge}
+                weight={game.weight}
+              />
+            );
+          })}
+        </Carousel>
     </Box>
     );
   }
--- ubg/src/pages/Home.js
+++ ubg/src/pages/Home.js
@@ -4,8 +4,6 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import {useFirestore} from 'reactfire';
-import GameCard from '../search/GameCard';
-import Carousel from 'react-elastic-carousel';
 import GameCategory from '../home/GameCategory';
 
 const useStyles = makeStyles((theme) => ({
@@ -23,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
  */
 export default function Home() {
   const classes = useStyles();
-  const [games, setGames] = React.useState([]);
+  const [topRated, setTopRated] = React.useState([]);
   const [fantasy, setFantasy] = React.useState([]);
   const [economic, setEconomic] = React.useState([]);
   const [cardGame, setCardGame] = React.useState([]);
@@ -37,15 +35,16 @@ export default function Home() {
  */
 function loadData() {
   if (initialize === false) {
-    const gameArr = [];setInitialize(true);
-    ref.orderBy(sortBy, 'desc').limit(10)
+    const topRatedArr = [];setInitialize(true);
+    ref.orderBy(sortBy, 'desc').limit(12)
         .get()
         .then((querySnapshot) => {
           
           querySnapshot.forEach((doc) => {
-            gameArr.push(doc.data());
+            topRatedArr.push(doc.data());
           });
-          setGames(gameArr);
+          // setGames(gameArr);
+          setTopRated(topRatedArr);
         })
         .catch(function(error) {
           console.log('Error getting documents: ', error);
@@ -54,7 +53,6 @@ function loadData() {
     ref.orderBy('weight').limit(12)
         .get()
         .then((querySnapshot) => {
-          // setInitialize(true);
           querySnapshot.forEach((doc) => {
             beginnerArr.push(doc.data());
           });
@@ -63,32 +61,31 @@ function loadData() {
         .catch(function(error) {
           console.log('Error getting documents: ', error);
         });
-    // const fantasyArr = [];
-    // const economicArr = [];
-    // const cardGameArr = [];
-    // ref.orderBy('rating')
-    //     .get()
-    //     .then((querySnapshot) => {
-    //       // setInitialize(true);
-    //       querySnapshot.forEach((doc) => {
-    //         if (doc.data()['categories'].includes('Fantasy') && fantasyArr.length < 6) {
-    //           fantasyArr.push(doc.data());
-    //         }
-    //         if (doc.data()['categories'].includes('Economic') && economicArr.length < 6) {
-    //           economicArr.push(doc.data());
-    //         }
-    //         if (doc.data()['categories'].includes('Card Game') && cardGameArr.length < 6) {
-    //           cardGameArr.push(doc.data());
-    //         }
+    const fantasyArr = [];
+    const economicArr = [];
+    const cardGameArr = [];
+    ref.orderBy('rating')
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            if (doc.data()['categories'].includes('Fantasy') && fantasyArr.length < 6) {
+              fantasyArr.push(doc.data());
+            }
+            if (doc.data()['categories'].includes('Economic') && economicArr.length < 6) {
+              economicArr.push(doc.data());
+            }
+            if (doc.data()['categories'].includes('Card Game') && cardGameArr.length < 6) {
+              cardGameArr.push(doc.data());
+            }
             
-    //       });
-    //       setFantasy(fantasyArr);
-    //       setEconomic(economicArr);
-    //       setCardGame(cardGameArr);
-    //     })
-    //     .catch(function(error) {
-    //       console.log('Error getting documents: ', error);
-    //     });
+          });
+          setFantasy(fantasyArr);
+          setEconomic(economicArr);
+          setCardGame(cardGameArr);
+        })
+        .catch(function(error) {
+          console.log('Error getting documents: ', error);
+        });
   }
 }
 useEffect(loadData, [initialize]);
@@ -108,44 +105,11 @@ useEffect(loadData, [initialize]);
           Welcome to UltimateBoardGame!
         </Typography>
       </Box>
-      <Box
-        className={classes.box}
-        container='true'
-        m={10}
-      >
-        <Typography variant='h5'>
-          Top-Rated Games
-        </Typography>
-        <br />
-        <Carousel
-          itemPadding={[10, 15]}
-          itemsToShow={5}
-          itemsToScroll={5}
-        >
-          {games.map((game) => {
-            return (
-              <GameCard className={classes.card}
-                key={game.id}
-                id={game.id}
-                image={game.image}
-                name={game.Name}
-                year={game.year}
-                minTime={game.minPlaytime}
-                maxTime={game.maxPlaytime}
-                minPlayer={game.minPlayer}
-                maxPlayer={game.maxPlayer}
-                rating={game.rating}
-                minAge={game.minAge}
-                weight={game.weight}
-              />
-            );
-          })}
-        </Carousel>
-      </Box>
+      <GameCategory category={'Top Rated'} games={topRated}/>
       <GameCategory category={'For Beginners'} games={beginner}/>
-      {/* <GameCategory category={'Fantasy'} games={fantasy}/>
+      <GameCategory category={'Fantasy'} games={fantasy}/>
+      <GameCategory category={'Card Game'} games={cardGame}/>
       <GameCategory category={'Economic'} games={economic}/>
-      <GameCategory category={'Card Game'} games={cardGame}/> */}
     </div>
   );
 }
