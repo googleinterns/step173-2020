0c84bb4bc86272e3dc28c33b79db29a8b0de7e4b
Shandy Kim <shandyjkim@gmail.com>
2020-08-06 13:51:59 -0700

    Move SettingsModal into mafia

--- ubg/src/common/WaitingRoom.js
+++ ubg/src/common/WaitingRoom.js
@@ -3,7 +3,7 @@ import {makeStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
-import SettingsModal from './SettingsModal';
+import SettingsModal from '../mafia/SettingsModal';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
