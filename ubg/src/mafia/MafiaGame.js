d4eb3edc8af5a0766f1bbd3528e7e2669a0616ed
zhangshyue <shiyuez@google.com>
2020-08-06 19:53:53 -0500

    show all players

--- ubg/src/mafia/MafiaGame.js
+++ ubg/src/mafia/MafiaGame.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import MafiaDay from './MafiaDay';
 import MafiaNight from './MafiaNight';
 import PropTypes from 'prop-types';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    width:'100%',
+  },
+}));
 
 /**
  * @return {ReactElement} Mafia game element
  */
 export default function MafiaGame({day}) {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       {
         day ?
         <MafiaDay /> :
--- ubg/src/mafia/MafiaNight.js
+++ ubg/src/mafia/MafiaNight.js
@@ -1,12 +1,120 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import {useParams} from 'react-router-dom';
+import {
+  useFirestore,
+  useFirestoreDocData,
+  // useUser,
+  useFirestoreCollectionData,
+  // useAuth,
+} from 'reactfire';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import UserVideo from '../common/UserVideo';
+// import Snackbar from '@material-ui/core/Snackbar';
+// import IconButton from '@material-ui/core/IconButton';
+// import CloseIcon from '@material-ui/icons/Close';
+import Player from './Player';
+import PersonalInfo from './PersonalInfo';
+// import firebase from 'firebase/app';
+// import Paper from '@material-ui/core/Paper';
+// import ChatIcon from '@material-ui/icons/Chat';
+// import Chat from '../common/Chat';
+// import NotFound from '../pages/NotFound';
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    height: '100vh',
+  },
+  video: {
+    background: theme.palette.primary.main,
+    textAlign: '-webkit-center',
+  },
+  gameContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+  },
+  text: {
+    
+  },
+  card: {
+    width: '100%',
+  },
+}));
 
 /**
  * @return {ReactElement} Mafia night element
  */
 export default function MafiaNight() {
+  const classes = useStyles();
+  const ref = useFirestore().collection('games');
+  const [initialize, setInitialize] = React.useState(false);
+  const [players, setPlayers] = React.useState([]);
+  const {roomId} = useParams();
+  const room = useFirestore().collection('rooms').doc(roomId);
+  const roomData = useFirestoreDocData(room);
+  const usersCollection = room.collection('users');
+  const usersData = useFirestoreCollectionData(usersCollection);
+
+  /**
+ * @return {undefined}
+ */
+function loadData() {
+  // using a hack to load data and reset filters when needed
+  if (initialize === false) {
+    setInitialize(true);
+    console.log(roomData);
+    console.log(usersData);
+    const allPlayers = [];
+    usersData.forEach(function(user) {
+      if (user.alive === true) {
+          allPlayers.push(user);
+      }
+  });
+  setPlayers(allPlayers);
+  }
+}
+/**
+ * Load the games data according to filter
+ */
+useEffect(loadData, [initialize]);
   return (
-    <div>
-      night
-    </div>
+    <Grid className={classes.gameContainer} item >
+
+                <PersonalInfo name={'shiyue'} role={'detective'} alive={true}/>
+                <Box m={10}>
+                  <Box className={classes.text} my={15} justify="center" mx="auto">
+                    <h2>Choose a person to kill...</h2>
+                    
+                  </Box>
+                  <Grid container justify="center" alignItems="center" spacing={4}>
+                  {
+                      players.map((u) => {
+                        return (
+                          <Player
+                          key={u.uid}
+                            name={u.displayName}
+                            // role={u.role}
+                          />
+                        );
+                      })
+                    }
+                  {/* <Player name={'shiyue'}/>
+                  <Player name={'shandy'} />
+                  <Player name={'daniel'} />
+                  <Player name={'jingya'} />
+                  <Player name={'morgan'} />
+                  <Player name={'shiyue'}/>
+                  <Player name={'shandy'} />
+                  <Player name={'daniel'} />
+                  <Player name={'jingya'} />
+                  <Player name={'morgan'} /> */}
+                  </Grid>
+                </Box>
+              </Grid>
+    
   );
 }
