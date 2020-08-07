8eaeffd41e0b657d9c814ea2c19cc32f29c1588f
Daniel Trevino <danytq00@gmail.com>
2020-08-07 14:23:17 -0500

    start day function

--- ubg/src/mafia/MafiaDay.js
+++ ubg/src/mafia/MafiaDay.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 /**
  * @return {ReactElement} Mafia day element
  */
-export default function MafiaDay() {
+export default function MafiaDay({mafiaKill, doctorSave, 
+  usersData, usersCollection}) {
+
+  function startDay() {
+    if(mafiaKill && mafiaKill.uid !== doctorSave.uid){
+      usersCollection.doc(mafiaKill).update({alive: false});
+      // send chat message or popup or snackbar for user killed
+    }
+  }
+
+  useEffect(startDay, []);
+
   return (
     <div>
       day
