2ee9ee3cf2ef3ab9bc1d48ac85768b051df8e64a
zhangshyue <shiyuez@google.com>
2020-08-14 17:10:29 -0500

    delete comments

--- ubg/src/common/RulesDrawer.js
+++ ubg/src/common/RulesDrawer.js
@@ -3,7 +3,6 @@ import Slide from '@material-ui/core/Slide';
 import {makeStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   rules: {
