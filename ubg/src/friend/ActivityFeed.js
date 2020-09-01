d7bbb26742944b5f5c33316c1c49c1c433990201
zhangshyue <shiyuez@google.com>
2020-09-01 10:33:49 -0500

    display add review in activity feed

--- ubg/src/friend/ActivityFeed.js
+++ ubg/src/friend/ActivityFeed.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
-import {useUser, useFirestoreDocData} from 'reactfire';
-import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import {useUser, useFirestore, useFirestoreDocData} from 'reactfire';
+import * as firebase from 'firebase/app';
+
+const useStyles = makeStyles((theme) => ({
+  fonts: {
+    display: 'inherit',
+  },
+}));
 
 /**
  * Renders users activity feed
@@ -12,8 +23,9 @@ import PropTypes from 'prop-types';
  */
 export default function ActivityFeed() {
   const user = useUser();
+  const classes = useStyles();
   const [initialize, setInitialize] = useState(false);
-  const [activites, setActivities] = useState([]);
+  const [activities, setActivities] = useState([]);
   const ref = useFirestore().collection('users').doc(user.uid);
   const userActivities = useFirestoreDocData(ref).activities;
 
@@ -26,28 +38,33 @@ function loadData() {
     const allActivities = [];
     userActivities.forEach(
       activity => 
-      {const newActicity = [];
-        const difference = (activity.timestamp - Date.now()) / (1000*60*60);
-        if (Math.floor(difference / 24) < 4) {
-          if (Math.floor(difference / 24) < 1) {
-            newActicity.push(Math.floor(difference) + "hours ago");
+      {const newActivity = [];
+        const difference = (Date.now() - activity.timestamp) / (1000*60);
+        if (Math.floor(difference / (60*24)) < 4) {
+          if (Math.floor(difference / (60*24)) < 1) {
+            if (Math.floor(difference / 60) < 1) {
+              newActivity.push(Math.floor(difference) + " minutes ago");
+            } else {
+            newActivity.push(Math.floor(difference / 60) + " hours ago");
+            }
           } else {
-            newActicity.push(Math.floor(difference / 24) + "days ago");
+            newActivity.push(Math.floor(difference / (60*24)) + " days ago");
           }
         } else {
           ref.update({
             activities: firebase.firestore.FieldValue.arrayRemove(activity),
           });
-          continue;
+          return;
         }
-      if (activity.type == "review") {
-        newActivity.push("left a review for" + game);
+      if (activity.type === "review") {
+        newActivity.push(" left a review for " + activity.game);
       } else {
-        newActivity.push("added" + game + "to favorites");
+        newActivity.push(" added " + activity.game + " to favorites");
       }
-      newActicity.push(activity.uid);
-      newActicity.push(activity.displayName);
-      allActivities.unshift(newActicity);
+      newActivity.push(activity.uid);
+      newActivity.push(activity.displayName);
+      allActivities.unshift(newActivity);
+      console.log(newActivity);
       }
     );
     setActivities(allActivities);
@@ -62,13 +79,33 @@ useEffect(loadData, [initialize]);
     <div>
       <List width="100%">
         {activities.map((activity) =>
-          <ListItem key={user.id}>
-            <Link href={'/profile/' + activity[2]}>
-              {activity[3]}
-            </Link>
-            {activity[1]}
-            {activity[0]}
-            <Divider />
+        
+          <ListItem key={activity[2] + activity[0]}  alignItems="flex-start">
+            <ListItemIcon>
+              <AddCircleOutlineIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={
+                <Typography
+                  // variant='h6'
+                  component={'span'}
+                  className={classes.fonts}
+                >
+                  <Link href={'/profile/' + activity[2]}>
+                    {activity[3]}
+                  </Link>
+                  {activity[1]}
+                </Typography>
+              }
+              secondary={
+                <Typography
+                  component={'span'}
+                >
+                  {activity[0]}
+                </Typography>
+              }
+            />
+            
           </ListItem>,
         )}
       </List>
